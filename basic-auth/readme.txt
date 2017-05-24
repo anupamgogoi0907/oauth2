799826f8658931cc7c16dd9a1092ae48f9361abd
ac-agogoi <anupamgogoi0907@gmail.com>
2017-05-24 19:52:37 -0300

    read me added

---
+++ basic-auth/readme.txt
@@ -0,0 +1,5 @@
+#Authorization Code
+http://localhost:8080/oauth/authorize?grant_type=authorization_code&response_type=code&client_id=mule&redirect_uri=http://localhost*
+
+#Access Token
+http://localhost:8080/oauth/token?code=4AISnB&redirect_uri=http://localhost*&grant_type=authorization_code&client_id=mule&client_secret=mulesecret
--- basic-auth/src/main/java/com/anupam/security/AuthServer.java
+++ basic-auth/src/main/java/com/anupam/security/AuthServer.java
@@ -5,6 +5,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
@@ -21,5 +22,9 @@ class AuthServer extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("mule").secret("mulesecret").authorizedGrantTypes("authorization_code").scopes("read", "write");
+    }
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
     }
 }
