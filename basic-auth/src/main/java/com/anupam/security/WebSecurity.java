8a314b5b67db7606342901a98f86cea3601c2d34
ac-agogoi <anupamgogoi0907@gmail.com>
2017-05-24 20:05:05 -0300

    formatted

--- basic-auth/src/main/java/com/anupam/security/WebSecurity.java
+++ basic-auth/src/main/java/com/anupam/security/WebSecurity.java
@@ -1,54 +1,33 @@
 package com.anupam.security;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import javax.servlet.http.HttpServletRequest;
-
 
 @EnableWebSecurity(debug = true)
 public class WebSecurity {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("admin").password("1234").roles("ADMIN").build());
-        return manager;
-    }
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("admin").password("1234").roles("ADMIN").build());
+		return manager;
+	}
 
+	@Configuration
+	public class BasicAuth extends WebSecurityConfigurerAdapter {
 
-    @Configuration
-    public class BasicAuth extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.antMatcher("/oauth/authorize").authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		}
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            http.antMatcher("/basic").authorizeRequests().anyRequest().authenticated().and().httpBasic();
-            http.antMatcher("/oauth/authorize").authorizeRequests().anyRequest().authenticated().and().httpBasic();
-        }
-
-
-    }
-
-//    @Configuration
-//    public class FormAuth extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.csrf().disable();
-//            http.authorizeRequests().antMatchers("/form").authenticated().and().formLogin();
-//        }
-//    }
+	}
 
 }
